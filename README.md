601b4a5c1e4e84b9b8eef1e0ceedd3f671e38821
Dmitriy Kotov <dmkotov@edu.hse.ru>
2020-04-29 02:43:35 +0300

    Beta version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# CiscoMarathon" 
+
+Homework N1
+
+Uses Netmiko and TextFSM libraries
+
+Set all parameters in hw1.py file
--- hw1.py
+++ hw1.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
 # """Module docstring."""
 
-#Imports
+# Imports
 from netmiko import ConnectHandler
 import csv
 import logging
 import datetime
-import multiprocessing as mp
-import difflib
-import filecmp
 import sys
 import os
+import time
 
-#Module 'Global' variables
-DEVICE_FILE_PATH = 'devices.csv' # file should contain a list of devices in format: ip,username,password,device_type
-BACKUP_DIR_PATH = 'backups' # complete path to backup directory
+DEVICE_FILE_PATH = 'devices.csv'  # file should contain a list of devices in format: ip,username,password,device_type
+BACKUP_DIR_PATH = 'backups'  # complete path to backup directory
+NTP_SERVER = '10.10.0.1'  # ntp server ip address
+
+TEXT_FSM_TEMPLATES_PATH = 'ntc-templates\\templates'  # templates path. Comment this line if you want to use default values
+
+try:
+    os.environ.setdefault("NET_TEXTFSM", TEXT_FSM_TEMPLATES_PATH)
+except NameError:
+    pass
+
 
 def enable_logging():
     # This function enables netmiko logging for reference
 
-    logging.basicConfig(filename='test.log', level=logging.DEBUG)
+    logging.basicConfig(filename='netmiko.log', level=logging.DEBUG)
     logger = logging.getLogger("netmiko")
+
 
 def get_devices_from_file(device_file):
     # This function takes a CSV file with inventory and creates a python list of dictionaries out of it
@@ -38,24 +45,18 @@ def get_devices_from_file(device_file):
         for row in reader:
             device_list.append(row)
 
-    print ("Got the device list from inventory")
-    print('-*-' * 10)
-    print ()
-
     # returning a list of dictionaries
     return device_list
+
 
 def get_current_date_and_time():
     # This function returns the current date and time
     now = datetime.datetime.now()
 
-    print("Got a timestamp")
-    print('-*-' * 10)
-    print()
-
     # Returning a formatted date string
     # Format: yyyy_mm_dd-hh_mm_ss
     return now.strftime("%Y_%m_%d-%H_%M_%S")
+
 
 def connect_to_device(device):
     # This function opens a connection to the device using Netmiko
@@ -63,27 +64,28 @@ def connect_to_device(device):
 
     # Since there is a 'hostname' key, this dictionary can't be used as is
     connection = ConnectHandler(
-        host = device['ip'],
-        username = device['username'],
+        host=device['ip'],
+        username=device['username'],
         password=device['password'],
         device_type=device['device_type'],
         secret=device['secret']
     )
 
-    print ('Opened connection to '+device['ip'])
-    print('-*-' * 10)
-    print()
+    print('Opened connection to ' + device['ip'])
+
 
     # returns a "connection" object
     return connection
 
+
 def disconnect_from_device(connection, hostname):
-    #This function terminates the connection to the device
+    # This function terminates the connection to the device
 
     connection.disconnect()
-    print ('Connection to device {} terminated'.format(hostname))
+    print('Connection to device {} terminated'.format(hostname))
 
-def get_backup_file_path(hostname,timestamp):
+
+def get_backup_file_path(hostname, timestamp):
     # This function creates a backup file name (a string)
     # backup file path structure is hostname/hostname-yyyy_mm_dd-hh_mm
 
@@ -93,12 +95,13 @@ def get_backup_file_path(hostname,timestamp):
 
     # Merging a string to form a full backup file name
     backup_file_path = os.path.join(BACKUP_DIR_PATH, hostname, '{}-{}.txt'.format(hostname, timestamp))
-    print('Backup file path will be '+backup_file_path)
-    print('-*-' * 10)
-    print()
+    print('Backup file path will be ' + backup_file_path)
+
+
 
     # returning backup file path
     return backup_file_path
+
 
 def create_backup(connection, backup_file_path, hostname):
     # This function pulls running configuration from a device and writes it to the backup file
@@ -113,8 +116,8 @@ def create_backup(connection, backup_file_path, hostname):
         with open(backup_file_path, 'w') as file:
             file.write(output)
         print("Backup of " + hostname + " is complete!")
-        print('-*-' * 10)
-        print()
+
+
 
         # if successfully done
         return True
@@ -125,78 +128,62 @@ def create_backup(connection, backup_file_path, hostname):
         return False
 
 
-def get_previous_backup_file_path(hostname, curent_backup_file_path):
-    # This function looks for the previous backup file in a directory
-    # Requires a hostname and the latest backup file name as an input
+def check_cdp_neighbours_count(connection):
+    # This function executes sh cdp neighbors details function and returns formatted result
 
-    # removing the full path
-    current_backup_filename = curent_backup_file_path.split(os.path.sep)[-1]
+    output = connection.send_command('sh cdp neighbors detail', use_textfsm=True)
 
-    # creatting an empty dictionary to keep backup file names
-    last_backup_file = sorted(filter(lambda f: f != current_backup_filename, os.listdir(os.path.dirname(curent_backup_file_path))), key=lambda f: f.split("-"), reverse=True)
-    return os.path.dirname(curent_backup_file_path) + os.path.sep + last_backup_file[0] if last_backup_file else False
-    '''
-    # looking for previous backup files
-    for file_name in os.listdir(os.path.join(BACKUP_DIR_PATH, hostname)):
-
-        # select files with correct extension and names
-        if file_name.endswith('.txt') and file_name != current_backup_filename:
-
-            # getting backup date and time from filename
-            filename_datetime = datetime.datetime.strptime(file_name.strip('.txt')[len(hostname)+1:],'%Y_%m_%d-%H_%M_%S')
-
-            # adding backup files to dict with key equal to datetime in unix format
-            backup_files[filename_datetime.strftime('%s')] = file_name
-
-    if len(backup_files) > 0:
-
-        # getting the previous backup filename
-        previous_backup_key = sorted(backup_files.keys(), reverse=True)[0]
-        previous_backup_file_path = os.path.join(BACKUP_DIR_PATH, hostname, backup_files[previous_backup_key])
-
-        print("Found a previous backup ", previous_backup_file_path)
-        print('-*-' * 10)
-        print()
-
-        # returning the previous backup file
-        return previous_backup_file_path
+    if type(output) == list:
+        return f"CDP is ON, {len(output)} peers"
     else:
+        return f"CDP is OFF, 0 peers"
+
+
+def check_version(connection):
+    # This function parses sh ver command
+
+    output = connection.send_command('sh ver', use_textfsm=True)
+
+    if not output:
+        return "", "", "", ""
+
+    return output[0].get("hostname", ""), \
+           output[0].get("hardware", [""])[0], \
+           output[0].get("version", "").upper(), \
+           "NPE" if "NPE" in output[0].get("running_image", "").upper() else "PE"
+
+
+def set_timezone(connection):
+    # set timezone to +0
+
+    print("Setting timezone to +0")
+    connection.send_config_set(["clock timezone GMT+0 0"])
+
+
+def ping_ntp(connection, ntp_server):
+    # checking ntp server availability
+
+    print(f"Ping NTP server {ntp_server}")
+    output = connection.send_command(f'ping {ntp_server}\n')
+    if "....." in output:
+        print(f"NTP server is not available")
         return False
-    '''
-
-
-def compare_backup_with_previous_config(previous_backup_file_path, backup_file_path):
-    # This function compares created backup with the previous one and writes delta to the changelog file
-    # Requires a path to last backup file and a path to the previous backup file as an input
-
-    # creating a name for changelog file
-    changes_file_path = backup_file_path.strip('.txt') + '.changes'
-
-    # checking if files differ from each other
-    if not filecmp.cmp(previous_backup_file_path, backup_file_path):
-        print('Comparing configs:')
-        print('\tCurrent backup: {}'.format(backup_file_path))
-        print('\tPrevious backup: {}'.format(previous_backup_file_path))
-        print('\tChanges: {}'.format(changes_file_path))
-        print('-*-' * 10)
-        print()
-
-        # if they do differ, open files in read mode and open changelog in write mode
-        with open(previous_backup_file_path,'r') as f1, open(backup_file_path,'r') as f2, open(changes_file_path,'w') as f3:
-            # looking for delta
-            delta = difflib.unified_diff(f1.read().splitlines(),f2.read().splitlines())
-            # writing discovered delta to the changelog file
-            f3.write('\n'.join(delta))
-        print ('\tConfig state: changed')
-        print('-*-' * 10)
-        print()
-
     else:
-        print('Config was not changed since the latest version.')
-        print('-*-' * 10)
-        print()
+        print("Ok!")
+        return True
 
-def process_target(device,timestamp):
+
+def check_ntp(connection, ntp_server):
+    # check if clock is in sync
+    print(f"Promoting ntp server {ntp_server}")
+    connection.send_config_set([f"ntp server {ntp_server}"])
+#    connection.send_command(["clock read-calendar"])
+    time.sleep(4)
+    output = connection.send_command("sh ntp asso")
+    return "Clock in Sync" if f"*~{ntp_server}" in output else "Not Sync"
+
+
+def process_target(device, timestamp):
     # This function will be run by each of the processes in parallel
     # This function implements a logic for a single device using other functions defined above:
     #  - connects to the device,
@@ -207,23 +194,25 @@ def process_target(device,timestamp):
     # Requires connection object and a timestamp string as an input
 
     connection = connect_to_device(device)
-    
+
     backup_file_path = get_backup_file_path(device['hostname'], timestamp)
-    backup_result = create_backup(connection, backup_file_path, device['hostname'])
-    
+
+    create_backup(connection, backup_file_path, device['hostname'])
+
+    _cdp_result = check_cdp_neighbours_count(connection)
+
+    _hostname, _device_type, _image, _is_NPE = check_version(connection)
+
+    set_timezone(connection)
+
+    ping_ntp(connection, NTP_SERVER)
+
+    _ntp_result = check_ntp(connection, NTP_SERVER)
+
+
     disconnect_from_device(connection, device['hostname'])
 
-    # if the script managed to create a backup, then look for a previous one
-    if backup_result:
-        previous_backup_file_path = get_previous_backup_file_path(device['hostname'], backup_file_path)
-
-        # if the previous one exists, compare
-        if previous_backup_file_path:
-            compare_backup_with_previous_config(previous_backup_file_path, backup_file_path)
-        else:
-            print('Unable to find previos backup file to find changes.')
-            print('-*-' * 10)
-            print()
+    return f"{_hostname}|{_device_type}|{_image}|{_is_NPE}|{_cdp_result}|{_ntp_result}"
 
 def main(*args):
     # This is a main function
@@ -236,32 +225,21 @@ def main(*args):
 
     # getting a device list from the file in a python format
     device_list = get_devices_from_file(DEVICE_FILE_PATH)
-    '''
-    # creating a empty list
-    processes=list()
 
-    # Running workers to manage connections
-    with mp.Pool(4) as pool:
-        # Starting several processes...
-        for device in device_list:
-            processes.append(pool.apply_async(process_target, args=(device,timestamp)))
-        # Waiting for results...
-        for process in processes:
-            process.get()
-    '''
+    # creating a empty list
+    processes = list()
+
+
+    result = []
     for device in device_list:
-        process_target(device, timestamp)
+        result.append(process_target(device, timestamp))
+
+    print(*result, sep="\n")
+
 
 if __name__ == '__main__':
     # checking if we run independently
     _, *script_args = sys.argv
-    
+
     # the execution starts here
     main(*script_args)
-
-
-
-
-
-
-
